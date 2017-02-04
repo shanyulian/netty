4745ec7cddcd793cc223d07c3371b72228158bb9
shan <shanyulian@aliyun.com>
2017-02-04 11:31:24 +0800

    添加出毛巾的指令

--- src/main/java/com/haro/netty/iot/netty/sendbuf/OutTowel.java
+++ src/main/java/com/haro/netty/iot/netty/sendbuf/OutTowel.java
@@ -9,5 +9,12 @@ package com.haro.netty.iot.netty.sendbuf;
 * @date 2017年1月16日 下午3:49:17
  */
 public class OutTowel {
+//出毛巾代码
+      byte sendBuy[];
+
+    public  static  void  outTowelBuf(byte[] buy){
+
+       // return sendBuy;
+    }
 
 }
